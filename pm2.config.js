2aadb5e325bf97fdbb772d0a8c5f968d91e48815
Gauravst <gauravsinghtangariya1@gmail.com>
2026-01-18 13:58:16 +0530

    added pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,15 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "frontend",
-      script: "npx",
-      args: "serve -s dist -l 5173",
-      cwd: "/var/www/dupload/client",
-      autorestart: true,
-      watch: false,
-    },
-    {
-      name: "backend",
+      name: "dupload_backend",
       script: "npm",
       args: "run start",
       cwd: "/var/www/dupload/server",
